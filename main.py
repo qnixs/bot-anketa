e56bd58dfc555b3adc513ca228c0675b7b6f16b2
qnixs <artemevrov@gmail.com>
2024-01-05 15:15:56 +0500

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from info import user_data
 
 TOKEN = '6769332141:AAExVUnMzcjrooAmmevPVysALd_4lVJCjyE'
 bot = telebot.TeleBot(TOKEN)
@@ -6,7 +7,14 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "hi")
+    bot.send_message(message.chat.id, "i")
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_id = message.from_user.id
+    if user_id not in user_data:
+        bot.send_message(message.chat.id, "Пожалуйста, пришли сначала свой возраст.")
 
 
 bot.polling()
